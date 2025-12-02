14dd3e152f6dbba44096d320bcb0399c1f446a31
xdNecron <venomdushii@gmail.com>
2025-12-02 21:15:12 +0100

    d2: tweaks

--- task-2/part2.py
+++ task-2/part2.py
@@ -1,8 +1,7 @@
 #!/usr/bin/env python3
 
 with open("input.txt", "r") as f:
-    inputs = f.read()
-    proc = [rng.split("-") for rng in inputs.split(",")]
+    proc = [rng.split("-") for rng in f.read().split(",")]
 
 _sum = 0
 
@@ -10,8 +9,7 @@ for low, high in proc:
     for i in range(int(low), int(high) + 1):
         string = str(i)
         for j in range(1, len(string) // 2 + 1):
-            substr = string[0:j]
-            occs = string.split(substr)
+            occs = string.split(string[:j])
             if set(occs) == {""}:
                 _sum += i
                 break
